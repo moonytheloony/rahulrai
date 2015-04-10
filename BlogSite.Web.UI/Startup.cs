0c3897c10be7ed0180ee5a2dbdd98c31b1eff4d0
Rahul Rai <rahulrai@microsoft.com>
2015-04-11 00:14:31 +0530

    Template file and folders

---
+++ BlogSite.Web.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BlogSite.Web.UI.Startup))]
+namespace BlogSite.Web.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

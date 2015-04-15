651ccf67eb8c875f7067a0794680f690b833ed37
Rahul Rai <rahulrai@microsoft.com>
2015-04-15 14:06:55 +0530

    adding unchecked project files

---
+++ RahulRai.Websites.Utilities.Web/ControllerFactory.cs
@@ -0,0 +1,35 @@
+﻿#region
+
+using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.SessionState;
+
+#endregion
+
+namespace RahulRai.Websites.Utilities.Web
+{
+    public class ControllerFactory : IControllerFactory
+    {
+        public IController CreateController(RequestContext requestContext, string controllerName)
+        {
+            //ILogger logger = new DefaultLogger();
+            //var controller = new HomeController(logger);
+            //return controller;
+            throw new NotImplementedException();
+        }
+
+        public SessionStateBehavior GetControllerSessionBehavior(
+            RequestContext requestContext, string controllerName)
+        {
+            return SessionStateBehavior.Default;
+        }
+
+        public void ReleaseController(IController controller)
+        {
+            var disposable = controller as IDisposable;
+            if (disposable != null)
+                disposable.Dispose();
+        }
+    }
+}

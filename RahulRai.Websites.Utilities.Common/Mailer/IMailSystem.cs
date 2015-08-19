9a63dd7acdbe416c038663e2e4bf4ac01af60aac
moonytheloony <rahulrai@live.com>
2015-08-19 14:27:17 +0530

    Modified layout of connect button

---
+++ RahulRai.Websites.Utilities.Common/Mailer/IMailSystem.cs
@@ -0,0 +1,23 @@
+﻿// ***********************************************************************
+// Assembly         : RahulRai.Websites.Utilities.Common
+// Author           : rahulrai
+// Created          : 08-18-2015
+//
+// Last Modified By : rahulrai
+// Last Modified On : 08-18-2015
+// ***********************************************************************
+// <copyright file="IMailSystem.cs" company="Rahul Rai">
+//     Copyright (c) Rahul Rai. All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+
+namespace RahulRai.Websites.Utilities.Common.Mailer
+{
+    /// <summary>
+    /// Interface IMailSystem
+    /// </summary>
+    internal interface IMailSystem
+    {
+    }
+}
